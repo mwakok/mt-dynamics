f215acbed9c23dd1f0436982fcbea397d513e965
florian-huber <36473328+florian-huber@users.noreply.github.com>
2018-11-14 11:10:09 +0100

    minor code cleaning

--- simulation_functions.py
+++ simulation_functions.py
@@ -1,6 +1,5 @@
 """
 Function to run and initialize the MT simulation
-
 @author: FlorianHuber
 """
 
@@ -8,7 +7,7 @@ import simulation_parameters as simpa
 from parameters import ParameterSet
 import csv
 import json
- 
+import numpy as np
 
 
 def dict_to_json(mydict, file_json): 
@@ -25,13 +24,11 @@ def json_to_dict(file_json):
     return mydict
     
 
-
 def dict_to_csv(mydict, file_csv):
     # Writes dictionary to csvfile
     csvfile = csv.writer(open(file_csv, 'w', newline=''))#, quoting=csv.QUOTE_NONNUMERIC)
     for key in mydict.keys():
         csvfile.writerow((key,mydict[key], type(mydict[key])))
-
 
 
 def csv_to_dict(file_csv):
@@ -46,22 +43,23 @@ def csv_to_dict(file_csv):
     return mydict
 
 
-def load_parameters():
-    # add all parameters to one dictionary
-
-#    if np.where(simpa.EB_growth_speed[:,0] == 0.2)[0].shape[0] == 1:
-#        growth_speed = simpa.EB_growth_speed[np.where(simpa.EB_growth_speed[:,0] == simpa.EB),1]
-#    else:
-#        print('No growth speed found for given EB concentration')
-#    sPARAM['growth_speed'] = growth_speed
-#    print('Growth speed set to: ', growth_speed)
-
-    simParameters = simpa.simParameters
-    simParameters['growth_rate_one'] = growth_speed/(60*simpa.dL_dimer) #rate of one dimer per s
+# TODO:
+#def load_parameters():
+#    # add all parameters to one dictionary
+#
+##    if np.where(simpa.EB_growth_speed[:,0] == 0.2)[0].shape[0] == 1:
+##        growth_speed = simpa.EB_growth_speed[np.where(simpa.EB_growth_speed[:,0] == simpa.EB),1]
+##    else:
+##        print('No growth speed found for given EB concentration')
+##    sPARAM['growth_speed'] = growth_speed
+##    print('Growth speed set to: ', growth_speed)
+#
+#    simParameters = simPa.simParameters
+#    simParameters['growth_rate_one'] = simpa.growth_speed/(60*simPa.dL_dimer) #rate of one dimer per s
     
     
 def analyse_EB_signal(simPa, EB_comet_sum, barrier_contact_times):
-    
+
     # Select valid runs
     b = []
     for a in range(0,len(EB_comet_sum)):
@@ -72,7 +70,7 @@ def analyse_EB_signal(simPa, EB_comet_sum, barrier_contact_times):
     max_barrier_contact_frames = int(round(np.max(barrier_contact_times/simPa.frame_rate_actual),0)) 
     min_length_run_frames = int(min_length_run/frame_rate_actual)
     frame_window = min_length_run_frames + max_barrier_contact_frames
-     
+
     EB_signal = np.zeros((len(valid_runs), frame_window+1)) #min_length_run+1+max_barrier_contact)) #put individual runs into one np.array
     normalize_EB_signal = np.zeros(frame_window+1) #min_length_run+1+max_barrier_contact)
     for a in range(0,len(valid_runs)):
