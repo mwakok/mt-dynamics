efbaf15a744ef59f32543294cf14acfcd0e3445e
florian-huber <36473328+florian-huber@users.noreply.github.com>
2019-12-17 11:50:23 +0100

    updated or added liscence

--- simulation_parameters.py
+++ simulation_parameters.py
@@ -1,7 +1,7 @@
 #
 # mt-dynamics
 #
-# Copyright 2019 Florian Huber
+# Copyright 2019 Florian Huber, Maurits Kok
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- simulation_screening.py
+++ simulation_screening.py
@@ -1,5 +1,20 @@
-# License to add here...
-
+#
+# mt-dynamics
+#
+# Copyright 2019 Florian Huber, Maurits Kok
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from parameters import ParameterSet
 import numpy as np
@@ -301,7 +316,6 @@ class simulation_screen(object):
                                         cmap='gist_heat_r', interpolation='nearest')
                         ax.set_ylabel(scan_y_label)
                         ax.set_xlabel(scan_x_label)
-    #                    ax.set_title(Scenario_titles[count])
                         fig.colorbar(map, ax = ax, label = 'sqrt(MSE)')
                
                     ax.set_xticks(np.arange(dim1))
@@ -341,7 +355,6 @@ class simulation_screen(object):
 
                         ax.set_ylabel(scan_y_label)
                         ax.set_xlabel(scan_x_label)
-#                        ax.set_title(Scenario_titles[count])
                         fig.colorbar(map, ax = ax, label = 'sqrt(MSE)')#'P (Kolmogorov-Smirnov)')
                
                     ax.set_xticks(np.arange(dim1))
@@ -381,7 +394,6 @@ class simulation_screen(object):
                                         aspect='auto', cmap='gist_heat_r', interpolation='nearest')
                         ax.set_ylabel(scan_y_label)
                         ax.set_xlabel(scan_x_label)
-#                        ax.set_title(Scenario_titles[count])
                         fig.colorbar(map, ax = ax, label = 'sqrt(MSE*MSE)')#'P (Kolmogorov-Smirnov)')
                
                     ax.set_xticks(np.arange(dim1))
